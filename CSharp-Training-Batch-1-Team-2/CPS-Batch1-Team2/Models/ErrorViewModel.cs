0e770fd42d428d23dc064ba6f60a8fb33a917c01
RAJNEESH RAI <146312745+therajneesh1@users.noreply.github.com>
2025-03-17 13:26:12 +0530

    Add files via upload

---
+++ CSharp-Training-Batch-1-Team-2/CPS-Batch1-Team2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPS_Batch1_Team2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
